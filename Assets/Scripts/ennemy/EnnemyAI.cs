7bae495727da3df2375fb7821ead87669795b1c1
Tristan <turgytristan@gmail.com>
2023-03-17 15:47:28 +0100

    presque finsh shalah

--- Assets/Scripts/ennemy/EnnemyAI.cs
+++ Assets/Scripts/ennemy/EnnemyAI.cs
@@ -15,26 +15,61 @@ public class EnnemyAI : MonoBehaviour
     [SerializeField]
     private int TaDistanceQueTuConsidèreAssezPetite = 3;
 
+    [SerializeField]
+    private int TaDistanceQueTuConsidèreAssezPetite2 = 30;
+
 
     public Transform target;
+
+    [SerializeField]
+    public Transform[] destinations; // tableau de destinations
+
 
     void Start()
     {// Find the player character
         target = GameObject.FindGameObjectWithTag("Player").transform;
         agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        StartCoroutine(ChangeDestinationCoroutine());
     }
 
     void Update()
     {
 
-        if (target != null)
-        {
-            agent.destination = target.position;
-        }
+
         if (Vector3.Distance(target.position, transform.position) < TaDistanceQueTuConsidèreAssezPetite)
         {
             m_OnTouch.Invoke();
         }
+        if (Vector3.Distance(target.position, transform.position) < TaDistanceQueTuConsidèreAssezPetite2)
+        {
+            if (target != null)
+            {
+                agent.destination = target.position;
+            }
+        }
+        if (Vector3.Distance(target.position, transform.position) >= TaDistanceQueTuConsidèreAssezPetite2)
+        {
+            StartCoroutine(ChangeDestinationCoroutine());
+        }
     }
-    
+    IEnumerator ChangeDestinationCoroutine()
+    {
+        int currentDestinationIndex = 0;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(15f); // attendre 15 secondes
+
+            currentDestinationIndex++;
+
+            if (currentDestinationIndex >= destinations.Length)
+            {
+                currentDestinationIndex = 0;
+            }
+
+            agent.destination = destinations[currentDestinationIndex].position;
+        }
+
+    }
+
 }
